bf4d3c568e420ffe080f75aa773f1fc817922398
dominhsang-34 <dosangnd2020@gmail.com>
2024-03-23 20:23:50 +0700

    first commit

---
+++ testing23.java
@@ -0,0 +1,16 @@
+public class testing23 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+
+    public static int addPositiveNumber(int a, int b){
+        if (a > 0 && b > 0 ) {
+            return a + b;
+        } else {
+            return 0;
+        }
+    }
+    
+}
